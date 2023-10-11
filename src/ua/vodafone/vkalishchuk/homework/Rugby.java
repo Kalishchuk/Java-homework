8b0c1a6cb0475a7fe1d5abaa961b4f63ced8f1da
VKalishchuk <k-vladimir-@ukr.net>
2023-10-11 12:25:07 +0300

    Rugby add new metod

--- src/ua/vodafone/vkalishchuk/homework/Rugby.java
+++ src/ua/vodafone/vkalishchuk/homework/Rugby.java
@@ -1,27 +1,35 @@
 package ua.vodafone.vkalishchuk.homework;
 
 import java.util.concurrent.ThreadLocalRandom;
+
 public class Rugby {
     public static void main(String[] args) {
         int countPlayers = 25;
-
-        System.out.println("Ages first team:  ");
         int[] firstTeam = randomPlayersAges(countPlayers);
-        System.out.println();
-        System.out.println("Ages second team: ");
         int[] secondTeam = randomPlayersAges(countPlayers);
+
+        System.out.print("Ages first team:  ");
+        displayAge(firstTeam);
+        System.out.println();
+        System.out.print("Ages second team: ");
+        displayAge(secondTeam);
 
         System.out.println();
         System.out.printf("Average arithmetic age of players = %.2f", arithmeticsAge(firstTeam));
         System.out.println();
         System.out.printf("Average arithmetic age of players = %.2f ", arithmeticsAge(secondTeam));
+    }
 
+    public static void displayAge(int[] age) {
+        for (int player : age) {
+            System.out.print(player + ", ");
+        }
     }
 
     public static double arithmeticsAge(int[] teamAges) {
         double sum = 0;
-        for (int num : teamAges) {
-            sum = sum + num;
+        for (int age : teamAges) {
+            sum = sum + age;
         }
         return sum / teamAges.length;
     }
@@ -30,7 +38,6 @@ public class Rugby {
         int[] ages = new int[teamSize];
         for (int i = 0; i < ages.length; i++) {
             ages[i] = ThreadLocalRandom.current().nextInt(18, 41);
-            System.out.print(ages[i] + "\t");
         }
         return ages;
     }
