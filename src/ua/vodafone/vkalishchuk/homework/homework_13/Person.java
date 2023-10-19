0f421cafa21b17454d5272876e8b56f81f1d8a35
VKalishchuk <k-vladimir-@ukr.net>
2023-10-19 13:32:20 +0300

    personInfo version1

--- src/ua/vodafone/vkalishchuk/homework/homework_13/Person.java
+++ src/ua/vodafone/vkalishchuk/homework/homework_13/Person.java
@@ -1,18 +1,52 @@
 package ua.vodafone.vkalishchuk.homework.homework_13;
 
 public class Person {
-    String name;
-    String surname;
-    String city;
-    String phone;
+    private String name;
+    private String surname;
+    private String city;
+    private String phone;
 
-    public String personInfo(String name, String surname, String city, String phone ){
-        this.name =name;
-        this.surname =surname;
-        this.city =city;
-        this.phone = phone;
+    //String name, String surname, String city, String phone
+
+    public String personInfo() {
+        this.setName(name);
+        this.setSurname(surname);
+        this.setCity(city);
+        this.setPhone(phone);
 
         return "You can call " + name + " " + surname + " from city " + city +
                 " by number phone " + phone;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
     }
 }
--- src/ua/vodafone/vkalishchuk/homework/homework_13/PersonRunner.java
+++ src/ua/vodafone/vkalishchuk/homework/homework_13/PersonRunner.java
@@ -4,26 +4,26 @@ public class PersonRunner {
     public static void main(String[] args) {
 
         Person firstPerson = new Person();
-        firstPerson.name = "Will";
-        firstPerson.surname = "Smith";
-        firstPerson.city = "New York";
-        firstPerson.phone = "2936729462846";
+        firstPerson.setName("Will");
+        firstPerson.setSurname("Smith");
+        firstPerson.setCity("New York");
+        firstPerson.setPhone("2936729462846");
 
         Person secondPerson = new Person();
-        secondPerson.name = "Jackie";
-        secondPerson.surname = "Chan";
-        secondPerson.city = "Shanghai";
-        secondPerson.phone = "12312412412";
+        secondPerson.setName("Jackie");
+        secondPerson.setSurname("Chan");
+        secondPerson.setCity("Shanghai");
+        secondPerson.setPhone("12312412412");
 
         Person thirdPerson = new Person();
-        thirdPerson.name = "Sherlock";
-        thirdPerson.surname = "Holmes";
-        thirdPerson.city = "London";
-        thirdPerson.phone = "37742123513";
+        thirdPerson.setName("Sherlock");
+        thirdPerson.setSurname("Holmes");
+        thirdPerson.setCity("London");
+        thirdPerson.setPhone("37742123513");
 
-        System.out.println(firstPerson.personInfo("Will", "Smith", "New York", "2936729462846"));
-        System.out.println(secondPerson.personInfo("Jackie", "Chan", "Shanghai", "12312412412"));
-        System.out.println(thirdPerson.personInfo("Sherlock", "Holmes", "London", "37742123513"));
+        System.out.println(firstPerson.personInfo());
+        System.out.println(secondPerson.personInfo());
+        System.out.println(thirdPerson.personInfo());
 
 
     }
