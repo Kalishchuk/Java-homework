4bc4522f71e134b8356b8b02fc0c3f93c7fe1090
VKalishchuk <k-vladimir-@ukr.net>
2023-10-23 12:30:12 +0300

    Step array version-1

--- src/ua/vodafone/vkalishchuk/homework/StepArray.java
+++ src/ua/vodafone/vkalishchuk/homework/StepArray.java
@@ -25,29 +25,85 @@ public class StepArray {
         System.out.println("############################################");
         System.out.println("Sorted arrays looks like: ");
         showArray(matrix);
-        System.out.println("############################################");
-
 
         System.out.println("############################################");
-        summaMatrix(matrix);
         System.out.println("Summa all int in array: " + summaMatrix(matrix));
         System.out.println("############################################");
+
+        int[] arrayMinimum = minElemetInMatrix(matrix);
+        System.out.println("The minimum elements for each row: ");
+        showArrayMinimum(arrayMinimum);
+
+        System.out.println("############################################");
+        int absolutMinimum = findMinimum(arrayMinimum);
+        System.out.println("Absolute minimum: " + absolutMinimum);
+        System.out.println("############################################");
+
+        System.out.println("Divide each element of the matrix by the absolute minimum: ");
+        if (absolutMinimum == 0) {
+            System.out.println("Absolute minimum is 0, division is not possible!");
+        } else {
+            devisionAllElementsMatrix(matrix, absolutMinimum);
+            showArray(matrix);
+            System.out.println("############################################");
+        }
+
+
+    }
+
+    private static void devisionAllElementsMatrix(int[][] matrix, int absolutMinimum) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                matrix[i][j] /= absolutMinimum;
+
+            }
+        }
+    }
+
+    private static void showArrayMinimum(int[] arrayMinimum) {
+        for (int element : arrayMinimum) {
+            System.out.print(element + " ");
+        }
+        System.out.println();
+    }
+
+    public static int[] minElemetInMatrix(int[][] matrix) {
+        int[] array = new int[matrix.length];
+
+        for (int i = 0; i < array.length; i++) {
+            if (matrix[i].length != 0) {
+                array[i] = findMinimum(matrix[i]);
+
+            } else {
+                System.out.println("There are empty elements in the matrix. It is impossible to find an absolute minimum");
+            }
+        }
+        return array;
+    }
+
+    public static int findMinimum(int[] lines) {
+        int min = lines[0];
+        for (int element : lines) {
+            if (element < min) {
+                min = element;
+            }
+        }
+        return min;
     }
 
     private static void sotrMatrix(int[][] matrix) {
         for (int i = 0; i < matrix.length; i++) {
-                if ((i + 1) % 2 == 0) {
-                    sortAsc(matrix[i]);
-                } else {
-                    sortDesc(matrix[i]);
-                }
+            if ((i + 1) % 2 == 0) {
+                sortAsc(matrix[i]);
+            } else {
+                sortDesc(matrix[i]);
             }
-
+        }
     }
 
     private static void sortDesc(int[] matrix) {
         for (int i = 0; i < matrix.length - 1; i++) {
-            for (int j = 0; j < matrix.length -1; j++) {
+            for (int j = 0; j < matrix.length - 1; j++) {
                 if (matrix[j] < matrix[j + 1]) {
                     swap(matrix, j);
                 }
@@ -57,7 +113,7 @@ public class StepArray {
 
     private static void sortAsc(int[] matrix) {
         for (int i = 0; i < matrix.length - 1; i++) {
-            for (int j = 0; j < matrix.length -1; j++) {
+            for (int j = 0; j < matrix.length - 1; j++) {
                 if (matrix[j] > matrix[j + 1]) {
                     swap(matrix, j);
                 }
